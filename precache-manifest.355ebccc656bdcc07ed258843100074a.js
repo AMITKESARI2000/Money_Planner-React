482be345b7c4db72ed89a3931baf9b8744741c90
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-08-16 12:19:39 +0530

    Updates

---
+++ precache-manifest.355ebccc656bdcc07ed258843100074a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0c202d8f7be06eef128b1e60479e81",
+    "url": "/Money_Planner-React/index.html"
+  },
+  {
+    "revision": "31239fdf0f7db9015c3e",
+    "url": "/Money_Planner-React/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/Money_Planner-React/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Money_Planner-React/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31239fdf0f7db9015c3e",
+    "url": "/Money_Planner-React/static/js/main.d199e32f.chunk.js"
+  },
+  {
+    "revision": "edffedd0ec5b4e5a4c87",
+    "url": "/Money_Planner-React/static/js/runtime-main.7e8fec6d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Money_Planner-React/precache-manifest.355ebccc656bdcc07ed258843100074a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Money_Planner-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
